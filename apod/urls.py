62ab7e2ea27b2eebf1b30b29f9291db70547a537
Martin Laine <martin.laine@gmail.com>
2011-10-31 20:08:49 +0000

    Added pagination to list

--- apod/urls.py
+++ apod/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('apod.views',
-	url(r'^$', 'list', name="list_view"),
-	url(r'^refresh/$', 'refresh'),
+	url(r'^$', 'list', name="first_page"),
+	url(r'^(?P<page>\d+)/$', 'list', name="page"),
 	url(r'^image/(?P<image_id>\d+)/$', 'image', name='image_view'),
 )
--- apod/views.py
+++ apod/views.py
@@ -1,27 +1,25 @@
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from apod.models import Photo
 from apod import apodapi
 
-def refresh(request):
-	try:
-		from_date = Photo.objects.latest().publish_date
-	except:
-		from_date = None
-	for apod_details in apodapi.get_archive_list(from_date=from_date):
-		photo = Photo(publish_date=apod_details["publish_date"], title=apod_details["title"])
-		photo.save()
-	
-	return redirect('list_view')
+def list(request, page=1):
+	all_photos  = Photo.objects.all()
 
-def list(request):
-	photos  = Photo.objects.filter(publish_date__year=1997, publish_date__month=9)
+	paginator = Paginator(all_photos, 25)
+
+	try:
+		photos = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		photos = paginator.page(1)
+
 	return render(request, 'apod/index.html', { 'photos': photos })
 
 def image(request, image_id):
 	photo = Photo.objects.get(pk=image_id)
 
-	photo.load_from_apod(False)
+	photo.load_from_apod(True)
 	
 	return render(request, 'apod/image.html', { 'photo': photo })
