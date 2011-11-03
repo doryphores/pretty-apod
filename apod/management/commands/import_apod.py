fc7403adcaf6005fbb24ab72255320d4d41fa06f
Martin Laine <martin.laine@gmail.com>
2011-11-03 23:25:48 +0000

    Added toolbar navigation and styled info panel

--- apod/management/commands/import_apod.py
+++ apod/management/commands/import_apod.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 		elif len(args) == 2:
 			photos_to_load = Photo.objects.filter(publish_date__year=int(args[0]), publish_date__month=int(args[1]))
 		else:
-			photos_to_load = Photo.objects.filter()
+			photos_to_load = Photo.objects.filter(loaded=False)
 			
 		#transaction.commit_unless_managed()
 		#transaction.enter_transaction_management()
