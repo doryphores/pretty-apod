cc4d217ce6dda01092156aa34bacd02bf151e8a4
Martin <martin.laine>
2012-09-07 16:36:38 +0100

    Added static file serving in development mode

--- urls.py
+++ urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^', include('apod.urls')),
-	
 	url(r'^admin/', include(admin.site.urls)),
 )
+
+# Serve static and media files in DEBUG mode
+# In production, these files will be served by the web server
+if settings.DEBUG:
+	urlpatterns += staticfiles_urlpatterns()
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
