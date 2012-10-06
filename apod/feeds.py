67e0280a665efd1a6cfe0e7c8ebabd49df095c3a
Martin Laine <martin.laine@gmail.com>
2012-10-06 12:31:53 +0100

    Added favicon to feeds

--- apod/feeds.py
+++ apod/feeds.py
@@ -1,6 +1,8 @@
 from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Atom1Feed
+from django.contrib.sites.models import get_current_site
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 
 from apod.models import Picture, Tag
 from apod.utils import format_http_date
@@ -8,16 +10,31 @@ from apod.utils import format_http_date
 ITEMS_LIMIT = 20
 
 
+class FeedGen(Atom1Feed):
+	def add_root_elements(self, handler):
+		super(FeedGen, self).add_root_elements(handler)
+		# Add icon root element
+		handler.addQuickElement('icon', self.feed['icon'])
+
+
 class LatestPicturesFeed(Feed):
-	feed_type = Atom1Feed
+	feed_type = FeedGen
 	title = 'Pretty APOD recent images'
 	link = '/'
 	description_template = 'feed_summary.html'
 
 	def __call__(self, request, *args, **kwargs):
+		# Set icon URL for use in feed_extra_kwargs
+		self.icon = 'http://%s%sfavicon.ico' % (get_current_site(request), settings.STATIC_URL)
 		response = super(LatestPicturesFeed, self).__call__(request, *args, **kwargs)
 		response['Last-Modified'] = format_http_date(self.items()[0].created_date)
 		return response
+
+	def feed_extra_kwargs(self, obj):
+		# Add icon to feed arguments
+		return {
+			'icon': self.icon
+		}
 
 	def items(self):
 		return Picture.objects.all()[0:ITEMS_LIMIT]
